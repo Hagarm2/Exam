7a4c53e088d633f3ee64e589fc05768d8796096d
dell <hajrm645@gmail.com>
2023-09-30 13:31:18 +0300

    Exam

--- lib/firstscreen.dart
+++ lib/firstscreen.dart
@@ -1,11 +1,251 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:exam/secondscreen.dart';
 import 'package:flutter/material.dart';
 
-class FirstScreen extends StatelessWidget {
-  static const String routename='firstScreen';
-  const FirstScreen({super.key});
+class FirstScreen extends StatefulWidget {
+  static const String routename = 'firstScreen';
+
+  //const FirstScreen({super.key});
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                children: [
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Image(
+                    image: AssetImage('assets/images/Group.png'),
+                    width: 30,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    'Moody',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(
+                    width: 160,
+                  ),
+                  Badge(
+                      offset: Offset(1, 5),
+                      child: Icon(
+                        Icons.notifications,
+                        color: Colors.black,
+                        size: 30,
+                      ))
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                children: [
+                  Text(
+                    "Hello ,",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                  ),
+                  Text(
+                    " Sara Rose",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                children: [
+                  Text(
+                    "How are you feeling today ?",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image(
+                    image: AssetImage('assets/images/Frame 14.png'),
+                    height: 88,
+                    width: 60,
+                  ),
+                  Image(
+                      image: AssetImage('assets/images/Frame 19.png'),
+                      height: 88,
+                      width: 60),
+                  Image(
+                      image: AssetImage('assets/images/Frame 15.png'),
+                      height: 88,
+                      width: 60),
+                  Image(
+                      image: AssetImage('assets/images/Frame 16.png'),
+                      height: 88,
+                      width: 60),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                children: [
+                  Text('Feature',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                      )),
+                  SizedBox(
+                    width: 160,
+                  ),
+                  Text(
+                    "See more",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                        color: Color(0xff027A48)),
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                    color: Color(0xff027A48),
+                    size: 30,
+                  )
+                ],
+              ),
+            ),
+            Container(
+                child: Center(
+                    child: CarouselSlider(
+              items: [
+                Image(
+                  image: AssetImage('assets/images/Frame 24.png'),
+                  //width: 950,
+                  fit: BoxFit.fitWidth,
+                ),
+                Image(
+                  image: AssetImage('assets/images/Frame 24.png'),
+                  //width: 950,
+                  fit: BoxFit.fitWidth,
+                ),
+                Image(
+                  image: AssetImage('assets/images/Frame 24.png'),
+                  // width: 950,
+                  fit: BoxFit.fitWidth,
+                )
+              ],
+              options: CarouselOptions(
+                autoPlay: true,
+                aspectRatio: 2.0,
+                enlargeCenterPage: true,
+
+                // autoPlayAnimationDuration: Duration(milliseconds: 800),
+                // autoPlayInterval: Duration(seconds: 3),
+                // enlargeFactor: 0.3,
+              ), // CarouselController:
+            ))),
+            /* Row(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(child: Icon(Icons.more_horiz,size: 50,)),
+              ],
+            ),*/
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                children: [
+                  Text('Exercise',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                      )),
+                  SizedBox(
+                    width: 160,
+                  ),
+                  Text(
+                    "See more",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                        color: Color(0xff027A48)),
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                    color: Color(0xff027A48),
+                    size: 30,
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image(image: AssetImage('assets/images/Frame 31.png')),
+                  Image(image: AssetImage('assets/images/Frame 35.png')),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image(
+                  image: AssetImage('assets/images/Frame 35 (1).png'),
+                ),
+                Image(image: AssetImage('assets/images/Frame 33.png')),
+              ],
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: Colors.green,
+              ),
+              label: "."),
+          BottomNavigationBarItem(
+              icon: InkWell(
+                onTap:(){ Navigator.pushNamed(context, SecondScreen.routename);},
+                  child: ImageIcon(
+                AssetImage('assets/images/grid-01.png'),
+                color: Colors.green,
+              )),
+              label: "."),
+          BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage('assets/images/calendar.png'),
+                  color: Colors.green),
+              label: "."),
+          BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage('assets/images/user-03.png'),
+                  color: Colors.green),
+              label: '.'),
+        ],
+        selectedItemColor: Colors.grey,
+        currentIndex: index,
+        onTap: (value) {
+          index = value;
+          setState(() {});
+        },
+      ),
+    );
   }
 }
--- lib/secondscreen.dart
+++ lib/secondscreen.dart
@@ -1,11 +1,163 @@
 import 'package:flutter/material.dart';
 
 class SecondScreen extends StatelessWidget {
-  static const String routename='secondScreen';
+  static const String routename = 'secondScreen';
+
   const SecondScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Colors.white,
+                      child: Image.asset(
+                        'assets/images/Ellipse 10.png',
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              "Hello, Jade",
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w400),
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "Ready to workout?",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.w600),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 100,
+                    ),
+                    Badge(
+                        offset: Offset(1, 5),
+                        child: Icon(
+                          Icons.notifications,
+                          color: Colors.black,
+                          size: 30,
+                        ))
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Container(
+                  width: 326,
+                  height: 82,
+                  color: Color(0xffF8F9FC),
+                  child: Row(
+
+                    children: [
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.heart_broken),
+                              Text("Heart Rate",style: TextStyle(fontWeight: FontWeight.w400,fontSize:12 ),),
+                            ],
+
+                          ),
+                          Row(
+                            children: [
+                              Text("81",style: TextStyle(fontWeight: FontWeight.w600,fontSize:18 ),),
+                              Text("BPM",style: TextStyle(fontWeight: FontWeight.w500,fontSize:14 ),)
+
+                            ],
+
+                          ),
+                        /*SizedBox(
+                           height: 50,
+                           child: VerticalDivider(
+                             thickness: 2,
+                             color: Colors.black.withOpacity(20),
+
+                            ),
+                         )*/
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.list),
+                              Text("To-do",style: TextStyle(fontWeight: FontWeight.w400,fontSize:12 ),),
+                            ],
+
+                          ),
+                          Row(
+                            children: [
+                              Text("32,5",style: TextStyle(fontWeight: FontWeight.w600,fontSize:18 ),),
+                              Text("%",style: TextStyle(fontWeight: FontWeight.w500,fontSize:14 ),)
+
+                            ],
+
+                          ),
+                          /*SizedBox(
+                           height: 50,
+                           child: VerticalDivider(
+                             thickness: 2,
+                             color: Colors.black.withOpacity(20),
+
+                            ),
+                         )*/
+                        ],
+                      ),
+
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'Workout Programs',
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                    )
+                  ],
+                ),
+              ),
+              TabBar(tabs: [
+                Text('All Type',style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500,color: Colors.black),),
+                Text('Full Body',style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500,color: Colors.black),),
+                Text('Upper',style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500,color: Colors.black),),
+                Text('Lower',style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500,color: Colors.black),),
+
+
+
+              ])
+
+            ],
+          ),
+          /*bottomNavigationBar: BottomNavigationBar(
+            items: [
+              //BottomNavigationBarItem(icon: icon)
+            ],
+          ),*/
+        ),
+      ),
+    );
   }
 }
